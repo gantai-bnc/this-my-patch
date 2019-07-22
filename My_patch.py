15e9c328cf74e94436c1ca09e0e171e38b28dee5
gantaiBNC <53185412+gantaiBNC@users.noreply.github.com>
2019-07-22 22:34:50 +0700

    My_bomberman.py

--- My_patch.py
+++ My_patch.py
@@ -1,4 +1,158 @@
+#!/system/xbin/bash
 clear
-Clear
+git clone https://github.com/amsitlab/smsid-java.git
+cd ./smsid-java
+chmod +x ./install
+apt install ./smsid_1.1_all.deb
+clear
 
-<p align="center"><h1>HELLO WORLD
+toilet -f slant --gay "bomberman"
+sleep 1
+echo "\033[33;1m Macam-macam tool untuk spam"
+sleep 1
+echo "\033[36;1m★""\033[34;1mgantai<bnc>""\033[36;1m★"
+sleep 1
+echo "\033[32;1m Spam Yang Tersedia :"
+echo "\033[35;1m1.""\033[36;1mBukalapak"
+echo "\033[35;1m2.""\033[36;1mTelkomsel"
+echo "\033[35;1m3.""\033[36;1mGrab"
+echo "\033[35;1m4.""\033[36;1mTokopedia"
+echo "\033[35;1m5.""\033[36;1mCodaShop"
+echo "\033[35;1m6.""\033[36;1mSurveyon"
+echo "\033[35;1m7.""\033[36;1mCustom"
+echo "\033[31;1m0. Keluar"
+echo "\033[37;1m81.""\033[30;1mInformasi Tool Ini"
+echo "\033[33;1m Pilih Angka:"
+read mrrm
+
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+toilet -f slant "Bukalapak"
+echo "\033[31;1mJenis Spam:""\033[37;1mChat WhatsApp"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+sleep 5
+clear
+cd $HOME/bomberman/Tul/
+php 1
+fi
+
+if
+[ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[31;1m"
+toilet "T-Sel"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 2.php
+fi
+
+if [ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Grab"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 3.php
+fi
+
+
+if [ $mrrm = 4 ] || [ $mrrm = 4 ]
+then
+clear
+toilet -f mono9 -F gay "Toked"
+echo "\033[31;1mJenis Spam:""\033[37;1mTelepon"
+echo "\033[31;1mBatas:""\033[37;1m1 Kali"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 4.php
+fi
+
+if [ $mrrm = 5 ] || [ $mrrm = 5 ]
+then
+clear
+toilet -f slant -F gay "CodaShop"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo
+echo
+sleep 1
+echo "\033[32;1mtunggu 5 Detik"
+sleep 5
+clear
+cd $HOME/LITESPAM/Tul/
+php 5.php
+fi
+
+if
+[ $mrrm = 6 ] || [ $mrrm = 6 ]
+then
+clear
+toilet -f standard -F gay "Surveyon "
+echo "\033[31;1mJenis Spam:""\033[37;1mEmail"
+echo "\033[31;1mBatas:""\033[37;1mUnlimited"
+echo "\033[31;1mPemograman:""\033[37;1mphp"
+echo "\033[31;1mAuthor:""\033[37;1mChandra Aditya"
+sleep 5
+cd $HOME/LITESPAM/Tul/
+php 6.php
+fi
+
+if
+[ $mrrm = 7 ] || [ $mrrm = 7 ]
+then
+clear
+toilet -f slant --gay "SMSID"
+echo "\033[31;1mJenis Spam:""\033[37;1mSMS"
+echo "\033[31;1mBatas:""\033[37;1m5"
+echo "\033[31;1mPemograman:""\033[37;1mBerjalan Langsung Pada DalvikVM"
+echo "\033[31;1mAuthor:""\033[37;1mAmsitlab"
+echo
+echo
+echo "\033[33;1mInformasi Lebih Lanjut hubungi:""\033[36;1m089630058754"
+sleep 6
+smsid boom -y
+fi
+
+if
+[ $mrrm = 81 ] || [ $mrrm = 81 ]
+then
+clear
+toilet -f slant --gay "bomberman"
+echo "\033[34;1mContact:""089630058754"
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+sleep 1
+echo "And All Member bnc secure"
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+sh LITESPAM.sh
+fi
+
+
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m dadah pantek :* "
+sleep 1
+fi
